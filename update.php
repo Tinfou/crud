f0940954320d232488a5c9785519f1b1ea8634f0
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-10-25 11:15:31 +0200

    ajout design framework bootstrap

--- update.php
+++ update.php
@@ -7,9 +7,23 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Modification</title>
+        <link rel="stylesheet" href="assets\css\bootstrap.min.css">
     </head>
     <body>
+        <div class="fluid">
+            <div class="bg-primary text-center">
+                <h3 class="p-2">CRUD PHP</h3>
+            </div>   
+        </div>
+        <div class="container p-2">
+            <nav>
+                <ul class="nav">
+                    <li><a class="nav-link" href="index.php">Listes des utilisateurs</a></li>
+                    <li><a class="nav-link" href="add.php">Ajouter des utilisateurs</a></li>
+                </ul>
+            </nav>
+        </div>
         <?php
             if(isset($_GET['id_user'])){
                 $id = $_GET['id_user'];
@@ -18,14 +32,31 @@
                 $result = mysqli_fetch_assoc($execute_query);
             }
         ?>
-        <form action="" method="post" enctype="multipart/form-data">
-            <p><input type="text" name="nom" value= "<?php echo $result['nom'] ?>"></p>
-            <p><input type="text" name="phone" value= "<?php echo $result['phone'] ?>"></p>
-            <p><input type="email" name="email"  value= "<?php echo $result['email'] ?>"></p>
-            <p><img src="images/<?php echo $result['image']; ?>" style="width: 60px; height: 60px;"><input type="file" name="image"></p>
-            <p><button type="submit" name ="submit">Modifier</button></p>
-        </form> 
-    <p><a href="index.php">Liste</a></p>
+        <div class="container p-4">
+            <div class ="d-flex justify-content-center">
+                <form class="g-3" action="" method="post" enctype="multipart/form-data">
+                    <div class="p-2 col-md-12">
+                        <label class="form-label" for="nom">Nom</label>
+                        <input class="form-control" type="text" name="nom" id="nom" value= "<?php echo $result['nom'] ?>">
+                    </div>
+                    <div class="p-2 col-md-12">
+                        <label class="form-label" for="email">Email</label>
+                        <input class="form-control" type="email" name="email" id="email" value= "<?php echo $result['email'] ?>">
+                    </div>
+                    <div class="p-2 col-md-12">
+                        <label class="form-label" for="phone">Phone</label>
+                        <input class="form-control" type="text" name="phone" id="phone" value= "<?php echo $result['phone'] ?>">
+                    </div>
+                    <div class="p-2 col-md-12 text-center">
+                        <label class="form-label" for="image">Photo de profil</label>
+                        <img class="rounded mx-auto d-block mb-2" src="images/<?php echo $result['image']; ?>" style="width: 60px; height: 60px;"><input class="form-control" type="file" name="image" id="image">
+                    </div>
+                    <div class="col-md-12">
+                        <button class="w-100 mt-3 btn btn-warning" type="submit" name="submit">Modifier</button>
+                    </div>
+                </form>
+            </div>
+        </div>   
     </body>
 </html>
 
